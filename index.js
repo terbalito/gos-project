692ca4f5fb0e1cd2fa2213e98ef04e65538981b9
Terbal <joelmoyo249@gmail.com>
2026-01-07 00:04:22 +0100

    changement de port pour laisser de la place à graphana

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const client = require("prom-client");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Prometheus registry
 const register = new client.Registry();
